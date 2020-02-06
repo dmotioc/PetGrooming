5d33ca1087ca257e41d79cc2856de14c231cf2bc
condrea098 <dmotioc@rosms.ro>
2020-02-06 19:25:44 +0200

    Adaugare model PetCategoryModel

---
+++ PetGroomingApplication/Models/PetCategoryModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetGroomingApplication.Models
+{
+    public class PetCategoryModel
+    {
+        public Guid IDPetCategory { get; set; }
+        public Categories Category { get; set; }
+        public Sizes Size { get; set; }
+        public SpeciesE Species { get; set; }
+
+    }
+
+    public enum Categories
+    {
+        NotSpecified =0,
+        LongHair = 1, 
+        MediumHair =2, 
+        ShortHair =3
+    }
+    public enum Sizes
+    {
+        NotSpecified = 0,
+        Small = 1,
+        Medium = 2,
+        Big = 3
+    }
+    public enum SpeciesE
+    {
+        NotSpecified = 0,
+        Dog = 1,
+        Cat = 2,
+        Rabbit = 3
+    }
+
+}
